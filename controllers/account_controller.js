cbf30b45288619cdf855814aad4e5bf50606b28e
cimex <cimex@cimex.com>
2025-04-10 15:06:53 -0700

    added ability to insert new order and added order appearing in dashboard

--- controllers/account_controller.js
+++ controllers/account_controller.js
@@ -1,15 +1,26 @@
+import { pool } from "../data/pool.js";
+
 export const renderAccount = async (req, res) => {
-    res.render("user-account-dashboard", { activeSection: "dashboard" });
+
+    res.render("user-account-dashboard", { activeSection: "dashboard"});
 };
 
 export const renderOrders = async (req, res) => {
-    res.render("user-account-dashboard", { activeSection: "orders" });
+    const userID = 777; // Hardcoded for now
+    const orders = await queryUserOrders(userID);
+    const groupedOrders = await sortOrderByPurchaseDate(orders);
+    console.log(groupedOrders);
+    
+    res.render("user-account-dashboard", { activeSection: "orders", groupedOrders });
 };
+
+
 
 export const renderSelling = async (req, res) => {
     res.render("user-account-dashboard", { activeSection: "selling" });
 };
 
+//Returns all order history from most recent to oldest
 export async function queryUserOrders(userID) {
     const [record] = await pool.query(`
     SELECT 
@@ -23,9 +34,32 @@ export async function queryUserOrders(userID) {
     FROM \`order\` o
     JOIN order_item oi ON o.orderID = oi.orderID
     JOIN item i ON oi.itemID = i.itemID
-    WHERE o.userID = 777;
+    WHERE o.userID = ?
+    ORDER BY o.purchase_date DESC;;
 
     `, [userID]);
 
     return record;
+}
+
+async function sortOrderByPurchaseDate(orders)
+{
+    const grouped = {};
+    orders.forEach(row => {
+    const date = row.purchase_date;
+    if (!grouped[date]) grouped[date] = [];
+    grouped[date].push({
+        orderID: row.orderID,
+        itemID: row.itemID,
+        item_name: row.item_name,
+        quantity: row.quantity,
+        price_at_purchase: row.price_at_purchase
+    });
+    });
+    const groupedOrders = Object.entries(grouped).map(([purchase_date, orders]) => ({
+        purchase_date,
+        orders
+      }));
+
+    return groupedOrders;
 }
--- routes/account_router.js
+++ routes/account_router.js
@@ -1,6 +1,7 @@
 import Router from "express";
 import { renderAccount, renderOrders, renderSelling } from "../controllers/account_controller.js";
 import { render } from "ejs";
+import { pool } from "../data/pool.js";
 export const accountRouter = Router();
 
 accountRouter.use((req, res, next) => {
